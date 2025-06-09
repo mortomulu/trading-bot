d8e43ff8e41899e04ce736ca6b1abe707bad171a
mortomulu <mfajrihidayat09@gmail.com>
2025-06-09 15:46:10 +0700

    feat: setup firebase & integrate to webhook api

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { placeOrder } from "../../lib/binance";
+import { placeOrder } from "@/lib/binance";
+import { db } from "@/lib/firebase";
+import { collection, addDoc, Timestamp } from "firebase/firestore";
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,22 +17,40 @@ export default async function handler(
     return res.status(400).json({ error: "Missing required fields" });
   }
 
+  const orderPayload: any = {
+    symbol,
+    side: action.toUpperCase() === "BUY" ? "BUY" : "SELL",
+    quantity,
+    type: type || "MARKET",
+  };
+
   try {
-    const result = await placeOrder({
-      symbol,
-      side: action.toUpperCase() === "BUY" ? "BUY" : "SELL",
-      quantity,
-      type: type || "MARKET",
+    const result = await placeOrder(orderPayload);
+
+    await addDoc(collection(db, "orders"), {
+      ...orderPayload,
+      status: "SUCCESS",
+      binanceResponse: result,
+      createdAt: Timestamp.now(),
+      from: "manual", 
     });
 
     return res.status(200).json({ message: "Order placed", result });
   } catch (err: any) {
-    console.error("Error placing order:", err.response?.data || err.message);
-    return res
-      .status(500)
-      .json({
-        error: "Order failed",
-        details: err.response?.data || err.message,
-      });
+    const errorData = err.response?.data || err.message;
+
+    await addDoc(collection(db, "orders"), {
+      ...orderPayload,
+      status: "FAILED",
+      error: errorData,
+      createdAt: Timestamp.now(),
+      from: "manual",
+    });
+
+    console.error("Error placing order:", errorData);
+    return res.status(500).json({
+      error: "Order failed",
+      details: errorData,
+    });
   }
 }
